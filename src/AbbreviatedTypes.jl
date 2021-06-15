54a9d411c120ccffde4bbb8e17900820a5222799
Wonseok Shin <wsshin97@gmail.com>
2021-06-15 14:08:43 -0400

    Rename CFloat to ComplexF, and XXXComplexYYY to XXXComplexFYYY

--- src/AbbreviatedTypes.jl
+++ src/AbbreviatedTypes.jl
@@ -5,20 +5,20 @@ module AbbreviatedTypes
 using Reexport
 @reexport using StaticArrays
 
-export Float, CFloat
+export Float, ComplexF
 export Tuple2, Tuple3, Tuple23
 export AbsVec, AbsMat, AbsArr
-export VecBool, VecInt, VecFloat, VecComplex
-export AbsVecBool, AbsVecInt, AbsVecFloat, AbsVecComplex, AbsVecInteger, AbsVecReal, AbsVecNumber
-export MatFloat, MatComplex, AbsMatFloat, AbsMatComplex, AbsMatReal, AbsMatNumber
-export ArrFloat, ArrComplex, AbsArrFloat, AbsArrComplex, AbsArrInteger, AbsArrNumber
-export SVec, SBool, SFloat, SInt, SInteger, SReal, SComplex, SNumber
-export SMat, SSBool, SSFloat, SSInt, SSInteger, SSReal, SSComplex, SSNumber
+export VecBool, VecInt, VecFloat, VecComplexF
+export AbsVecBool, AbsVecInt, AbsVecFloat, AbsVecComplexF, AbsVecInteger, AbsVecReal, AbsVecNumber
+export MatFloat, MatComplexF, AbsMatFloat, AbsMatComplexF, AbsMatReal, AbsMatNumber
+export ArrFloat, ArrComplexF, AbsArrFloat, AbsArrComplexF, AbsArrInteger, AbsArrNumber
+export SVec, SBool, SFloat, SInt, SInteger, SReal, SComplexF, SNumber
+export SMat, SSBool, SSFloat, SSInt, SSInteger, SSReal, SSComplexF, SSNumber
 
 ## Type aliases
 # Below, use Int instead of Int64 for compatibility with 32-bit systems (e.g., x86 in appveyor.yml).
 const Float = typeof(0.0)  # use Float = Float128 for quadruple precision in the future
-const CFloat = Complex{Float}
+const ComplexF = Complex{Float}
 
 const Tuple2 = NTuple{2}
 const Tuple3 = NTuple{3}
@@ -31,33 +31,33 @@ const AbsArr = AbstractArray
 const VecBool = Vector{Bool}
 const VecInt = Vector{Int}
 const VecFloat = Vector{Float}
-const VecComplex = Vector{CFloat}
+const VecComplexF = Vector{ComplexF}
 
 const AbsVecBool = AbsVec{Bool}
 const AbsVecInt = AbsVec{Int}
 const AbsVecFloat = AbsVec{Float}
-const AbsVecComplex = AbsVec{CFloat}
+const AbsVecComplexF = AbsVec{ComplexF}
 
 const AbsVecInteger = AbsVec{<:Integer}
 const AbsVecReal = AbsVec{<:Real}
 const AbsVecNumber = AbsVec{<:Number}
 
 const MatFloat = Matrix{Float}
-const MatComplex = Matrix{CFloat}
+const MatComplexF = Matrix{ComplexF}
 
 const AbsMatFloat = AbsMat{Float}
-const AbsMatComplex = AbsMat{CFloat}
+const AbsMatComplexF = AbsMat{ComplexF}
 
 const AbsMatReal = AbsMat{<:Real}
 const AbsMatNumber = AbsMat{<:Number}
 
 const ArrFloat{N} = Array{Float,N}
-const ArrComplex{N} = Array{CFloat,N}
+const ArrComplexF{N} = Array{ComplexF,N}
 
 # Below, without {N}, `where {T<:AbsArrNumber{3}}` is not equivalent to
 # `where {T<:AbstractArray{<:Number,3}}`.
 const AbsArrFloat{N} = AbsArr{Float,N}
-const AbsArrComplex{N} = AbsArr{CFloat,N}
+const AbsArrComplexF{N} = AbsArr{ComplexF,N}
 
 const AbsArrInteger{N} = AbsArr{<:Integer,N}
 const AbsArrNumber{N} = AbsArr{<:Number,N}
@@ -69,7 +69,7 @@ const SFloat{K} = SVec{K,Float}
 const SInt{K} = SVec{K,Int}
 const SInteger{K} = SVec{K,<:Integer}
 const SReal{K} = SVec{K,<:Real}
-const SComplex{K} = SVec{K,CFloat}
+const SComplexF{K} = SVec{K,ComplexF}
 const SNumber{K} = SVec{K,<:Number}
 
 # Static square matrices
@@ -79,7 +79,7 @@ const SSFloat{K,K²} = SMat{K,K²,Float}
 const SSInt{K,K²} = SMat{K,K²,Int}
 const SSInteger{K,K²} = SMat{K,K²,<:Integer}
 const SSReal{K,K²} = SMat{K,K²,<:Real}
-const SSComplex{K,K²} = SMat{K,K²,CFloat}
+const SSComplexF{K,K²} = SMat{K,K²,ComplexF}
 const SSNumber{K,K²} = SMat{K,K²,<:Number}
 
 end
